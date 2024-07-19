81fe18be62e9a8023fccaebbf4f3f057a2b3b6bd
Heliossandro Afonso <zenosama892@gmail.com>
2024-07-19 02:09:17 +0100

    Primeiro commit

---
+++ analise.java
@@ -0,0 +1,31 @@
+/*------------------------------------
+Tema: Gestão de um predio
+Nome: Heliossandro Afonso
+Numero: 947024057
+Ficheiro: Analise.java
+Data: 19.05.2024
+--------------------------------------*/
+
+/*
+1. Objectivo: cria um sistema, capaz de lidar com os problemas de um predio como, o fluxo de moradores, fluxo de trabalhadores, permitido a melhora da entrada e saida de dados.
+
+2. Visao [Interfaces Graficas]
+ - Login   
+ - MenuPrincipal
+ - MoradoresInterface
+ - FuncionariosInterface
+ - PagamentosInterface
+ - ArtigosInterface
+
+3. Entidades Fortes e Seus Atributos (Modelo)
+
+4. Ficheiro
+
+5. Tabelas de Apoio (Auxiliares) = Entidades Fracas
+  - Apartamentos.tab
+  - Andar.tab 
+  - Meses.tab
+
+6. Listagens e Pesquisas
+
+*/
